914f5fa11d34f8652c01ebd8341df3c54792c598
sun-fox <1999sunnykumarsingh@gmail.com>
2020-05-14 00:13:34 +0530

    Ready for rinkeby upload

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+const HDwalletProvider = require('truffle-hdwallet-provider');
+require('dotenv').config();
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -7,8 +9,12 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
-    // develop: {
-    //   port: 8545
-    // }
+    develop: {
+      port: 8545
+    },
+    rinkeby:{
+      provider:()=>new HDwalletProvider(process.env.MNEMONIC,"https://mainnet.infura.io/v3/"+process.env.INFURA_API_KEY),
+      network_id: 4
+    }
   }
 };
